4e039303f9ed520a880be68774aed14b19a4ff0b
misapavek <misapavek@seznam.cz>
2025-09-25 10:59:45 +0200

    asd

---
+++ src/main/java/org/example/bankAccounts/BaseBankAccount.java
@@ -0,0 +1,47 @@
+package org.example.bankAccounts;
+
+import org.example.people.BankAccountOwner;
+
+public class BasicBankAccount {
+
+    private String uuid;
+
+    private String accountNumber;
+
+    private BankAccountOwner owner;
+    double balance;
+
+    public BasicBankAccount(String uuid, String accountNumber, BankAccountOwner owner, double balance) {
+        this.uuid = uuid;
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public BankAccountOwner getOwner() {
+        return owner;
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+    public void AddBalance(int amount){
+        this.balance += amount;
+    }
+
+    public void SubtractBalance(int amount){
+        double subtraction = this.balance - amount;
+        if (subtraction > 0){
+            this.balance -= amount;
+        }
+    }
+
+
+}
